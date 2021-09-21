9cdfd42e111deea8592bc320b6f39d75df77710f
VASANTHAKUMAR B <vasanthakumar.b.17@mountblue.tech>
2021-09-21 14:45:46 +0530

    Adding question

--- problem1.js
+++ problem1.js
@@ -1,3 +1,11 @@
+/*
+    Problem 1:
+    
+    Using callbacks and the fs module's asynchronous functions, do the following:
+        1. Create a directory of random JSON files
+        2. Delete those files simultaneously 
+*/
+
 const fs = require('fs');
 
 function RandomFile(number) {
